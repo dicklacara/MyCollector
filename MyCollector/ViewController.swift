9f729ab13b551b8bc4d1d6db23b62d0afc1aaa48
Richard Applebaum <dicklacara@mac.com>
2016-08-25 09:54:51 -0700

    First commit

---
+++ MyCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCollector
+//
+//  Created by Richard Applebaum on 8/25/16.
+//  Copyright © 2016 La Cara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
